788fbe7e18042c63706bbc66d542051fd7b8aecf
Zen Yui <zenyui@gmail.com>
2018-03-08 17:05:10 -0500

    fuck auth

--- docker/neo4j-local.sh
+++ docker/neo4j-local.sh
@@ -2,10 +2,12 @@ case "$1" in
   start)
     docker run \
         --name some_neo4j \
-        --publish=7474:7474 --publish=7687:7687 \
+        --publish=7474:7474 \
+        --publish=7687:7687 \
         --volume=$HOME/neo4j/data:/data \
         --volume=$HOME/neo4j/logs:/logs \
-        neo4j:3.0
+        --env=NEO4J_dbms_security_auth__enabled=false \
+        neo4j:3.3
     ;;
   delete)
     docker kill some_neo4j
