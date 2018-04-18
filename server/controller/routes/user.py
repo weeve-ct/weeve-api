59ad31e134f8807b768448d152455def266a2781
Zen Yui <zenyui@gmail.com>
2018-04-18 18:13:57 -0400

    user endpoints stubs

---
+++ server/controller/routes/user.py
@@ -0,0 +1,18 @@
+from flask import request, jsonify
+from server.models import db, User
+from server.controller.security import SecureBlueprint
+from server.controller.errors import ValidationError, QueryError
+import logging
+
+logger = logging.getLogger(__name__)
+
+bp = SecureBlueprint('tag', __name__)
+
+@bp.route('/', methods=['GET'])
+@bp.route('/<id>', methods=['GET'])
+def get_user(user_id=None):
+    pass
+
+@bp.route('/', methods=['PUT', 'PATCH'])
+def edit_user():
+    pass
