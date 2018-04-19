c8353b790b4fd5e5b04e4b22fd95cfe90b0a0a40
Zen Yui <zenyui@gmail.com>
2018-04-19 11:40:35 -0400

    expand text preprocessor functions

--- server/controller/tokenizer.py
+++ server/controller/tokenizer.py
@@ -1,3 +1,19 @@
+import re
+
 def title_tokenizer(title):
     '''given title, return tokenized tags'''
-    return []
+    # naive split for now
+    return clean_whitespace(title).split(' ')
+
+def get_insensitive_unique(*args):
+    '''given N lists, return the case insensitive unique list'''
+    unique = {}
+
+    for array in args:
+        assert isinstance(array, (list, tuple)), 'function requires list or tuple'
+        unique.update({val.lower(): val for val in array})
+
+    return list(unique.values())
+
+def clean_whitespace(value):
+    return re.sub(r's+', ' ', value.strip())
