e289ccaebb5ca48b25075e17303034a85384bd9d
Zen Yui <zenyui@gmail.com>
2018-04-19 21:57:56 -0400

    user edits endpoint

--- server/controller/routes/user.py
+++ server/controller/routes/user.py
@@ -1,16 +1,14 @@
 import logging
-from flask import request, jsonify
+from flask import request, jsonify, g
 from server.models import db, User
 from server.controller.security import SecureBlueprint
 from server.controller.errors import ValidationError, QueryError, NotImplementedError
 
-
 logger = logging.getLogger(__name__)
-bp = SecureBlueprint('tag', __name__)
-
+bp = SecureBlueprint('user', __name__)
 
 @bp.route('/', methods=['GET'])
-@bp.route('/<id>', methods=['GET'])
+@bp.route('/<int:user_id>', methods=['GET'])
 def get_user(user_id=None):
     # get single user
     if user_id is not None:
@@ -24,7 +22,34 @@ def get_user(user_id=None):
         }
         return jsonify({'user': output})
 
+@bp.route('/<int:user_id>', methods=['PUT', 'PATCH'])
+def edit_user(user_id):
+    ValidationError.raise_assert(request.json, 'missing request json')
 
-@bp.route('/', methods=['PUT', 'PATCH'])
-def edit_user():
-    raise NotImplementedError('user edits not implemented yet')
+    body = request.json
+
+    user = db.session.query(User).filter_by(id=user_id).first()
+    QueryError.raise_assert(user is not None, 'user "{}" not found'.format(user_id))
+
+    # check that body user matches url user
+    if 'user_id' in request.json:
+        ValidationError.raise_assert(body['user_id']==user_id, \
+        'payload user_id different from url')
+
+    # check that token user == url user
+    if g['current_user'].user_id != user_id:
+        raise PermissionsError('not authorized to edit this user')
+
+    if 'username' in body:
+        user.username = body['username']
+
+    if 'display_name' in body:
+        user.display_name = body['display_name']
+
+    output = {
+        'user_id': user.id,
+        'username': user.username,
+        'display_name': user.display_name,
+        'picture': user.picture,
+    }
+    return jsonify({'user': output})
