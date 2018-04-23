0f266143c5437457b575aa0b16edd43363a0b145
Zen Yui <zenyui@gmail.com>
2018-04-22 21:26:11 -0400

    add lemmatizer, stop words, and custom tokenizer

--- server/controller/tokenizer.py
+++ server/controller/tokenizer.py
@@ -1,9 +1,24 @@
 import re
+import nltk
+import string
+
+# CONSTRUCT CUSTOM STOPWORDS
+STOPWORDS = set()
+for word in nltk.corpus.stopwords.words('english'):
+    STOPWORDS.add(word)
+    if "'" in word:
+        STOPWORDS.add(word.replace("'",""))
+STOPWORDS.add("'s")
+
+# build lemmatizer
+_LEMMATIZER = nltk.stem.WordNetLemmatizer()
+lemmatize = _LEMMATIZER.lemmatize
 
 def title_tokenizer(title):
     '''given title, return tokenized tags'''
     # naive split for now
-    return list(set(clean_whitespace(title).split(' ')))
+    tokens = get_tokens(title, STOPWORDS)
+    return list(set(x['output'] for x in tokens if not x.get('ignore',False)))
 
 def clean_whitespace(value):
     return re.sub(r'\s+', ' ', value.strip())
@@ -17,3 +32,27 @@ def get_insensitive_unique(*args):
         unique.update({val.lower(): val for val in array})
 
     return list(unique.values())
+
+def get_tokens(sentence, stopwords):
+    # identify POS
+    output = []
+    tokens = clean_whitespace(sentence)
+    tokens = nltk.tokenize.word_tokenize(tokens)
+
+    for ix, (word, pos) in enumerate(nltk.pos_tag(tokens)):
+        tmp = {'index': ix, 'original': word, 'pos': pos, 'output': word}
+
+        if word.lower() in stopwords:
+            tmp['ignore'] = True
+
+        elif word in string.punctuation:
+            tmp['ignore'] = True
+
+        # stem verbs
+        if pos.startswith('VB'):
+            tmp['output'] = lemmatize(word.lower(), pos='v')
+
+        # add to output
+        output.append(tmp)
+
+    return output
