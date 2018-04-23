cf096185e0025556202157f07bac14c9f635c049
Zen Yui <zenyui@gmail.com>
2018-04-22 21:34:04 -0400

    add has_explicit to tag model

--- server/models/tag.py
+++ server/models/tag.py
@@ -6,3 +6,4 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tag = db.Column(db.String(30), primary_key=False, unique=True, nullable=False)
     created_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+    has_explicit = db.Column(db.Boolean, nullable=False, default=True) # has an explicit use, not just implicit tags
