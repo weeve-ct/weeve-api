0429b0c2d90895eb5e6f1d0de67cfc129979313e
Zen Yui <zenyui@gmail.com>
2018-04-24 01:40:51 -0400

    split container and image commands

--- mysql.sh
+++ mysql.sh
@@ -3,9 +3,9 @@ VERSION=v0.7
 TAG_NAME=gcr.io/$PROJECT_ID/$IMG_NAME:$VERSION
 DOCKER_FOLDER=./docker
 
-DB_IMG_NAME=genie-db
+DB_IMG_NAME=weeve-db
 DB_TAG_NAME=gcr.io/$PROJECT_ID/$API_IMG_NAME:latest
-DB_CONTAINER_NAME=some-genie-db
+DB_CONTAINER_NAME=some-weeve-db
 
 case "$1" in
   clean)
@@ -19,11 +19,11 @@ case "$1" in
   build)
     echo "building image"
     docker build -t $DB_IMG_NAME -f $DOCKER_FOLDER/local_mysql.dockerfile .
-    echo "creating container"
-    docker create -p 3306:3306 --name=$DB_CONTAINER_NAME $DB_IMG_NAME
     ;;
 
   start)
+    echo "creating container"
+    docker create -p 3306:3306 --name=$DB_CONTAINER_NAME $DB_IMG_NAME
     echo "starting container"
     docker start $DB_CONTAINER_NAME
     ;;
@@ -33,10 +33,13 @@ case "$1" in
     docker stop $DB_CONTAINER_NAME
     ;;
 
-  delete)
+  kill)
     echo "deleting container"
     docker kill $DB_CONTAINER_NAME
     docker rm $DB_CONTAINER_NAME
+    ;;
+
+  delete)
     echo "deleting image"
     docker rmi $DB_IMG_NAME
     ;;
