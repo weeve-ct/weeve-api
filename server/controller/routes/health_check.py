f475e195902f5878a244622042b8cf2b20787b99
Zen Yui <zenyui@gmail.com>
2018-04-24 21:18:45 -0400

    health check logging

--- server/controller/routes/health_check.py
+++ server/controller/routes/health_check.py
@@ -1,12 +1,15 @@
 from flask import jsonify, Blueprint, request, current_app
 import time
 import socket
+import logging
 from server.controller import helpers
 
+logger = logging.getLogger(__name__)
 bp = Blueprint('health', __name__)
 
 @bp.route('/', methods=['GET'])
 def healthcheck():
+    logger.debug('health check')
     return jsonify({
         'code':'alive',
         'host': socket.gethostname(),
